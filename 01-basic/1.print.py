0d3b6522df44b03d129f60f18402a9e249d4712e
indunil-jay <indunil.jayz@gmail.com>
2025-02-07 00:15:29 +0530

    #1 add notes

--- 01-basic/1.print.py
+++ 01-basic/1.print.py
@@ -45,3 +45,48 @@ print("hello" + 40)   # Type Error : can only concatenate str (not "int") to str
 # Syntax errors aren't the only kind of problems you can run into when coding, for example:
         # 1.A bug in your logic. You code is valid, and will run, but it doesn't something unexpected.
         # 2.It's too slow. Your code is valid and does what's expected, but it does it slowly
+
+
+
+
+# The Zen of Python
+
+# Tim Peters, a long time Pythonista, describes the guiding principles of Python in his famous short piece, The Zen of Python.
+
+#     Beautiful is better than ugly.
+
+#     Explicit is better than implicit.
+
+#     Simple is better than complex.
+
+#     Complex is better than complicated.
+
+#     Flat is better than nested.
+
+#     Sparse is better than dense.
+
+#     Readability counts.
+
+#     Special cases aren't special enough to break the rules.
+
+#     Although practicality beats purity.
+
+#     Errors should never pass silently.
+
+#     Unless explicitly silenced.
+
+#     In the face of ambiguity, refuse the temptation to guess.
+
+#     There should be one-- and preferably only one --obvious way to do it.
+
+#     Although that way may not be obvious at first unless you're Dutch.
+
+#     Now is better than never.
+
+#     Although never is often better than right now.
+
+#     If the implementation is hard to explain, it's a bad idea.
+
+#     If the implementation is easy to explain, it may be a good idea.
+
+#     Namespaces are one honking great idea -- let's do more of those!
