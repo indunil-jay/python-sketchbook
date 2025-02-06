1815d0d2ce8e33abfe9cfe6a4a0ae9233d237360
indunil-jay <indunil.jayz@gmail.com>
2025-02-06 21:26:19 +0530

    #01:print function

---
+++ 01-basic/1.print.py
@@ -0,0 +1,47 @@
+#  print() function will print anything you put inside its parentheses.
+#-----------------------------------------------------------------------#
+
+# Each print() instruction prints on a new line.
+print('Hello world')  # 'hello world'
+print(40)  # 40
+
+
+#print function can do arithmatic operation before returning a result.
+print(40+1) # 41
+
+print('one'+'two') #onetwo
+
+print("hello" + 40)   # Type Error : can only concatenate str (not "int") to str
+
+
+
+#NOTE:
+# what is python ?
+   #simple answer is that it is a  high-level, general purpose programming language that is used for variety of purpose  inculding back-end development, data-analaysis, Machine learning, AI,etc.
+
+# Python is known for beign simple to read and write.
+
+# what is code  ?
+    #code is just a series of instruction for computer or (compiler to ) to follow  ro achive some certain task (work,software).
+
+# what is syntax erro in programming ?
+    #A syntax error is a mistake in a program's code that occurs when a programmer breaks the rules of the programming language. Syntax errors can be caused by:
+            # Misspelling commands
+            # Using punctuation incorrectly
+            # Incorrect labels
+            # Missing punctuation
+            # Misordered commands
+            # Missing semicolons
+            # Unbalanced parenthesis
+            # Missing operators
+            # Indentation
+            # Error in the structure of the program
+
+           # When a syntax error occurs, the code will not run and an error message will appear in the console. The error message will usually start with "SyntaxError: invalid syntax" and will indicate the location of the error in the code
+
+        # Syntax: The rules for how expressions and statements should be structured in a language.
+
+
+# Syntax errors aren't the only kind of problems you can run into when coding, for example:
+        # 1.A bug in your logic. You code is valid, and will run, but it doesn't something unexpected.
+        # 2.It's too slow. Your code is valid and does what's expected, but it does it slowly
