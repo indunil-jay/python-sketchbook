64eba896582c809edaa5c97b281c201eee0ff705
indunil-jay <indunil.jayz@gmail.com>
2025-02-07 22:11:23 +0530

    14execute ()

---
+++ 04-built-in-functions/14.exec.py
@@ -0,0 +1,13 @@
+#  execute is a extreamly powerful and dangerous function that allows you to run any kind steam code in python, scripts'
+
+
+user_input = """
+
+x= 10
+x+=1
+y= x * x
+
+print(y)
+
+"""
+exec(user_input)
