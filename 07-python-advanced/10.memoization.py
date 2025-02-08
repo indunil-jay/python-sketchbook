3ba13275ee70c842c250403039a6dbb468d6a8b3
indunil-jay <indunil.jayz@gmail.com>
2025-02-08 18:16:49 +0530

    memoizations

---
+++ 07-python-advanced/10.memoization.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from textwrap import wrap
+from time import perf_counter
+import sys
+
+
+def memoize(func):
+    cache: dict = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key: str = str(args) + str(kwargs)
+
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+
+        return cache[key]
+    return wrapper
+
+
+@memoize
+def fibonacci(n: int) -> int:
+    if (n < 2):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+start: float = perf_counter()
+f: int = fibonacci(30)
+end: float = perf_counter()
+
+print(f)
+print(f"Time : {end - start} seconds")
